9ca093eca0a596e6b572325dad114b55654c867d
Ethan Flower <ethanjamesflower@gmail.com>
2026-05-07 03:13:32 -0500

    refactor: delete MS-only packages (pairing, sync, crl, updatepoll)

--- internal/pairing/pem.go
+++
@@ -1,9 +0,0 @@
-package pairing
-
-import "encoding/pem"
-
-// decodePEMBlock is a thin wrapper that returns the first PEM block
-// and the remaining bytes. Used to walk multi-cert PEM bundles.
-func decodePEMBlock(b []byte) (*pem.Block, []byte) {
-	return pem.Decode(b)
-}
